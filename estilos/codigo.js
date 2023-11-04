97905591438a8d6b473dcf9cb9de69fd9ef2c315
sealar24 <s2arenhardt@gmail.com>
2023-11-03 21:46:38 -0300

    noviembreTresCuatro

--- estilos/codigo.js
+++ estilos/codigo.js
@@ -6,25 +6,32 @@ const ticket = 200;
 let precioProduccion = 95000;
 let precioHistoria = 85000;
 let precioNaturaleza = 90000;
-let elecPack
+let elecPack;
+let descuento;
+let 
 // tomar elementos
 
 const formulario = document.getElementById('formulario');
 const categoria = document.getElementById('categoria');
-const cantidad = document.getElementById('paquete');
+const paquete = document.getElementById('paquete');
 const totalPagar = document.getElementById('totalAPagar');
 const btnResumen = document.getElementById('resumen');
-
+const cantidad = document.getElementById('cantidad')
 
 //  condicionar por descuento
+if (cantidad>0){
+    cantidad=cantidad
+}else{
+    alert("Deve ingresar un numero valido" )
+}
 
-    switch (paquete.value) {
+switch (paquete.value) {
         case "produccion":
             elecPack=precioProduccion
             break;
             case "historia":
                 elecPack=precioHistoria
-            break;
+                break;
         case "naturaleza":
             elecPack=precioNaturaleza
             default:
@@ -32,14 +39,30 @@ const btnResumen = document.getElementById('resumen');
                 break;
             }
             
-            function calcularPago(){
-                let total = cantidad.value * elecPack;
+switch (categoria.value) {
+    case "familias":
+        descuento=0.12
+        break;
+    case  "escolares":
+        descuento=0.15
+        break;
+    case "Jubilados":
+        descuento=.20
+        break
 
-            }
-
-    totalPagar.textContent = `Total a Pagar: $ ${total}`;
-
+    default:
+        alert("deve seleccionar descuento")
+        break;
 }
+            function calcularPago(){
+                       let total = cantidad.value * elecPack;
+            
+                   }
+            
+            
+            totalPagar.textContent = `Total a Pagar: $ ${total}`;
+            
+
 
 btnResumen.addEventListener('click',(e)=>{
     e.preventDefault()
