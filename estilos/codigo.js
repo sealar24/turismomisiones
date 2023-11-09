4023ffa628560ae7b10d8b9a2d9b5ca273b26aa8
sealar24 <s2arenhardt@gmail.com>
2023-11-09 16:05:50 -0300

    noviembreNueve

--- estilos/codigo.js
+++ estilos/codigo.js
@@ -1,14 +1,8 @@
-// definir valor del ticket
-//const ticket = 200;
-
 // definir los descuentos
 
 let precioProduccion = 95000;
 let precioHistoria = 85000;
 let precioNaturaleza = 90000;
-let elecPack;
-let descuento;
-let 
 // tomar elementos
 
 const formulario = document.getElementById('formulario');
@@ -19,52 +13,53 @@ const btnResumen = document.getElementById('resumen');
 const cantidad = document.getElementById('cantidad')
 
 //  condicionar por descuento
-if (cantidad>0){
-    cantidad=cantidad
-}else{
-    alert("Deve ingresar un numero valido" )
-}
+// if (cantidad > 0) {
+//     cantidad = cantidad
+// } else {
+//     alert("Deve ingresar un numero valido")
+// }
 
-switch (paquete.value) {
+function calcularPago() {
+
+    let elecPack =0;
+let descuento =0;
+
+    switch (paquete.value) {
         case "produccion":
-            elecPack=precioProduccion
+            elecPack = precioProduccion
             break;
-            case "historia":
-                elecPack=precioHistoria
-                break;
+        case "historia":
+            elecPack = precioHistoria
+            break;
         case "naturaleza":
-            elecPack=precioNaturaleza
-            default:
-                total
-                break;
-            }
-            
-switch (categoria.value) {
-    case "familias":
-        descuento=0.12
-        break;
-    case  "escolares":
-        descuento=0.15
-        break;
-    case "Jubilados":
-        descuento=.20
-        break
+            elecPack = precioNaturaleza
+        default:
+            break;
+    }
 
-    default:
-        alert("deve seleccionar descuento")
-        break;
+    switch (categoria.value) {
+        case "familias":
+            descuento = 0.12
+            break;
+        case "escolares":
+            descuento = 0.15
+            break;
+        case "Jubilados":
+            descuento = 0.20
+            break
+
+        // default:
+        //     alert("deve seleccionar descuento")
+        //     break;
+    }
+    
+
+    let totalPago = cantidad.value * elecPack * (1 - descuento);
+    totalPagar.textContent = `Total a Pagar: $ ${totalPago}`;
+
 }
-            function calcularPago(){
-                       let total = cantidad.value * elecPack;
-            
-                   }
-            
-            
-            totalPagar.textContent = `Total a Pagar: $ ${total}`;
-            
 
-
-btnResumen.addEventListener('click',(e)=>{
-    e.preventDefault()
-    calcularPago()
+btnResumen.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcularPago();
 });
